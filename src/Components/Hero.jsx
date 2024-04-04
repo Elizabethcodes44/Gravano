1dfa00985ea4c634e1a4c8b8d0d33f0adc0899ab
Elizabeth <lizbeepmc@gmail.com>
2024-04-04 20:26:17 +0300

    resposiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
         <div className="Hero_Container" id = "hero">
             <div className="text">
             <h1>Gravano Ciana & co.</h1>
-            <p>Celebrate Life's Biggest Milestone with Us .<br></br> Find Your Dream Home Today. <br></br>Discover a World of Luxury, Comfort, and Serenity Awaits. <br></br>Welcome to Your Real Estate Journey Where Every Door Opens to Opportunity. <br></br>Let's Unlock the Door to Your Future Together .<br></br>Your Perfect Property Awaits. <br></br>Experience the Art of Real Estate and Craft Your Story of Home in Every Square Foot</p>
+            
             </div>
         </div>
     )
